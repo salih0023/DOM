555d323c489792a90db0b03014f301d4f842c919
As Salih <thickino@gmail.com>
2023-06-18 00:14:28 +0000

    commit avec les favicon

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
+
 let parent = document.querySelectorAll(".box")
-// console.log(parent)
 
 for(let i = 0; i < parent.length; i++){
     let btnPlus = parent[i].children[5].children[2]
@@ -9,9 +9,18 @@ for(let i = 0; i < parent.length; i++){
     let price = parent[i].children[3]
     let prix = parseInt(price.innerText)
     let btnMinus = parent[i].children[5].children[0]
-    // console.log(qty + 1)
+    //console.log(likrBtn)
+
+    //PlusBtn
     btnPlus.addEventListener("click", function(){
         qty++
+        quantity.innerText = qty 
+        price.innerText = prix * qty
+    })
+    //MinusBtn
+    btnMinus.addEventListener("click", function(){
+        if(qty > 1)
+        qty--
         quantity.innerText = qty 
         price.innerText = prix * qty
     })
